599a56925b2596f925bca505dc922cfdd7a8de29
trgiahuy14 <trgiahuy14@gmail.com>
2025-10-07 20:53:31 +0700

    feat: add email sending function using PHPMailer

--- includes/functions.php
+++ includes/functions.php
@@ -1,10 +1,49 @@
 <?php
-if(!defined('_HIENUE')){
+if (!defined('_HIENUE')) {
     die('Truy cập không hợp lệ');
 }
 
-function layout($layoutName, $data = []){
-    if (file_exists(_PATH_URL_TEMPLATES . '/layouts/' . $layoutName . '.php')){
+function layout($layoutName, $data = [])
+{
+    if (file_exists(_PATH_URL_TEMPLATES . '/layouts/' . $layoutName . '.php')) {
         require_once _PATH_URL_TEMPLATES . '/layouts/' . $layoutName . '.php';
+    }
+}
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+// Hàm gửi mail
+function sendMail($emailTo, $subject, $content)
+{
+
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'REDACTED';                     //SMTP username
+        $mail->Password   = 'REDACTED';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Recipients
+        $mail->setFrom('REDACTED', 'Admin');
+        $mail->addAddress($emailTo);     //Add a recipient
+
+
+        //Content
+        $mail->CharSet = 'UTF-8';
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = $subject;
+        $mail->Body    = $content;
+
+        return $mail->send();
+    } catch (Exception $e) {
+        echo "Gửi thất bại. Mailer Error: {$mail->ErrorInfo}";
     }
 }
--- index.php
+++ index.php
@@ -7,30 +7,36 @@ require_once './config.php';
 require_once './includes/connect.php';
 require_once './includes/database.php';
 require_once './includes/session.php';
+
+// Email
+require_once './includes/mailer/Exception.php';
+require_once './includes/mailer/PHPMailer.php';
+require_once './includes/mailer/SMTP.php';
+
+
 require_once './includes/functions.php';
 
 
-require_once './templates/layouts/index.php';
-
 // Mặc định là module dashboard, action là file index.php (chỉnh trong config.php)
+
 $module = _MODULES;
 $action = _ACTION;
 
 
 // Lấy path
-if(!empty($_GET['module'])){
+if (!empty($_GET['module'])) {
     $module = $_GET['module'];
 }
 
-if(!empty($_GET['action'])){
+if (!empty($_GET['action'])) {
     $action = $_GET['action'];
 }
 
 // Tạo đường dẫn
 $path = 'modules/' . $module . '/' . $action . '.php';
 // echo $path;
-if(!empty($path)){
-    if (file_exists($path)){
+if (!empty($path)) {
+    if (file_exists($path)) {
         require_once $path;
     } else {
         require './modules/errors/404.php';
