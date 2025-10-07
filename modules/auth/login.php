36842b363602e876d4d9af10f8bff57f7082ea7b
trgiahuy14 <trgiahuy14@gmail.com>
2025-10-07 17:29:08 +0700

    style: add bootstrap login page template

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -1,6 +1,58 @@
 <?php
 if(!defined('_HIENUE')){
     die('Truy cập không hợp lệ');
-} else {
-    echo 'Trang login';
 }
+
+require_once './templates/layout/header-auth.php';
+
+?>
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+        <img src="<?php echo _HOST_URL_TEMPLATES; ?>/assets/image/draw2.jpg"
+          class="img-fluid" alt="Sample image">
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <form>
+          <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+            <h2 class="fw-normal mb-5 me-3">Đăng nhập hệ thống</h2>
+            
+          </div>
+
+          <!-- Email input -->
+          <div data-mdb-input-init class="form-outline mb-4">
+            <input type="email" id="form3Example3" class="form-control form-control-lg"
+              placeholder="Địa chỉ email" />
+          </div>
+
+          <!-- Password input -->
+          <div data-mdb-input-init class="form-outline mb-3">
+            <input type="password" id="form3Example4" class="form-control form-control-lg"
+              placeholder="Nhập mật khẩu" />
+          </div>
+
+          <div class="d-flex justify-content-between align-items-center">
+            
+            <a href="#!" class="text-body">Quên mật khẩu?</a>
+          </div>
+
+          <div class="text-center text-lg-start mt-4 pt-2">
+            <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+              style="padding-left: 2.5rem; padding-right: 2.5rem;">Đăng nhập</button>
+            <p class="small fw-bold mt-2 pt-1 mb-0">Chưa có tài khoản? <a href="#!"
+                class="link-danger">Đăng ký</a></p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+  
+</section>
+
+<?php
+
+require_once './templates/layout/footer.php';
