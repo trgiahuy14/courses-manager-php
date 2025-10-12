27b2d41c2957900d8dab287922fa6b3b8f68eb26
trgiahuy14 <trgiahuy14@gmail.com>
2025-10-13 03:10:51 +0700

    feat(login): add form validate

--- modules/auth/login.php
+++ modules/auth/login.php
@@ -8,6 +8,47 @@ $data = [
 ];
 layout('header-auth', $data);
 
+
+if (!empty(isPost())) {
+  $filter = filterData();
+  $errors = [];
+  // Validate Email
+  if (empty(trim($filter['email']))) {
+    $errors['email']['required'] = 'Email bắt buộc phải nhập';
+  } else {
+    if (! validateEmail(trim($filter['email']))) {
+      $errors['email']['isEmail'] = 'Email không đúng định dạng';
+    }
+  }
+
+  // Validate password
+  if (empty($filter['password'])) {
+    $errors['password']['required'] = 'Mật khẩu bắt buộc phải nhập';
+  } else {
+    if (strlen(trim($filter['password']) < 6)) {
+      $errors['password']['length'] = 'Mật khẩu phải dài hơn 6 ký tự';
+    }
+  }
+
+  if (empty($errors)) {
+    setSessionFlash('msg', 'Dữ liệu hợp lệ');
+    setSessionFlash('msg_type', 'success');
+  } else {
+    setSessionFlash('msg', 'Vui lòng kiểm tra lại dữ liệu nhập vào');
+    setSessionFlash('msg_type', 'danger');
+    setSessionFlash('oldData', $filter);
+    setSessionFlash('errors', $errors);
+  }
+}
+
+
+
+
+
+$msg = getSessionFlash('msg');
+$msg_type = getSessionFlash('msg_type');
+$oldData = getSessionFlash('oldData');
+$errorsArr = getSessionFlash('errors');
 ?>
 
 <section class="vh-100">
@@ -18,7 +59,12 @@ layout('header-auth', $data);
           class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form>
+        <?php
+        if (!empty($msg) && !empty($msg_type)) {
+          getMsg($msg, $msg_type);
+        }
+        ?>
+        <form method="POST" action="" enctype="multipart/form-data">
           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
             <h2 class="fw-normal mb-5 me-3">Đăng nhập hệ thống</h2>
 
@@ -26,14 +72,26 @@ layout('header-auth', $data);
 
           <!-- Email input -->
           <div data-mdb-input-init class="form-outline mb-4">
-            <input type="email" id="form3Example3" class="form-control form-control-lg"
+            <input type="text" name="email" id="form3Example3"
+              value="<?php
+                      if (!empty($oldData)) {
+                        echo oldData($oldData, 'email');
+                      }  ?>"
+              class="form-control form-control-lg"
               placeholder="Địa chỉ email" />
+            <?php if (!empty($errorsArr)) {
+              echo formError($errorsArr, 'email');
+            } ?>
           </div>
 
           <!-- Password input -->
           <div data-mdb-input-init class="form-outline mb-3">
-            <input type="password" id="form3Example4" class="form-control form-control-lg"
+            <input type="password" name="password" id="form3Example4" class="form-control form-control-lg"
               placeholder="Nhập mật khẩu" />
+            <?php
+            if (!empty($errorsArr)) {
+              echo formError($errorsArr, 'password');
+            } ?>
           </div>
 
           <div class="d-flex justify-content-between align-items-center">
@@ -42,7 +100,7 @@ layout('header-auth', $data);
           </div>
 
           <div class="text-center text-lg-start mt-4 pt-2">
-            <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
+            <button type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg"
               style="padding-left: 2.5rem; padding-right: 2.5rem;">Đăng nhập</button>
             <p class="small fw-bold mt-2 pt-1 mb-0">Chưa có tài khoản? <a href="<?php echo _HOST_URL ?>?module=auth&action=register"
                 class="link-danger">Đăng ký</a></p>
