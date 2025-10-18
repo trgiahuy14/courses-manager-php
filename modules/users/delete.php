0aa50a59bca62a24c1ba7bb08d8fd11891e1a2a9
trgiahuy14 <trgiahuy14@gmail.com>
2025-10-19 01:29:33 +0700

    feat(users): add delete user

--- modules/users/delete.php
+++ modules/users/delete.php
@@ -1,4 +1,35 @@
 <?php
-if(!defined(_HIENUE)){
+if (!defined('_TRGIAHUY')) {
     die('Truy cập không hợp lệ');
+}
+
+$getData = filterData('get');
+
+if (!empty($getData['id'])) {
+    $user_id = $getData['id'];
+    $checkUser = getRows("SELECT * FROM users WHERE id = $user_id");
+    if ($checkUser > 0) {
+
+        // Check if is login
+        $checkToken = getRows("SELECT * FROM token_login WHERE user_id = $user_id");
+        if ($checkToken > 0) {
+            delete('token_login', "user_id = $user_id");
+        }
+
+        // Delete user
+        $checkDelete = delete('users', "id = $user_id");
+
+        if ($checkDelete) {
+            setSessionFlash('msg', 'Xóa người dùng thành công.');
+            setSessionFlash('msg_type', 'success');
+            redirect(('?module=users&action=list'));
+        } else {
+            setSessionFlash('msg', 'Xóa người dùng thất bại');
+            setSessionFlash('msg_type', 'danger');
+        }
+    } else {
+        setSessionFlash('msg', 'Người dùng không tồn tại.');
+        setSessionFlash('msg_type', 'danger');
+        redirect('?module=users&action=list');
+    }
 }
