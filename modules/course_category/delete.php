c8c2b3d8eac98b35aed209aa5ca111291e4f7760
trgiahuy14 <trgiahuy14@gmail.com>
2025-10-21 01:47:51 +0700

    feat(course_cate): add delete category

--- modules/course_category/delete.php
+++ modules/course_category/delete.php
@@ -2,3 +2,40 @@
 if (!defined('_TRGIAHUY')) {
     die('Truy cập không hợp lệ');
 }
+
+$filter = filterData('get');
+
+if (!empty($filter)) {
+    $cateID = $filter['id'];
+
+    $checkCate = getOne("SELECT * FROM course_category WHERE id = $cateID");
+
+    if (!empty($checkCate)) {
+        // check course table
+        $checkCourse = getRows("SELECT * FROM course WHERE category_id = $cateID");
+
+        if ($checkCourse > 0) {
+            // If course is exist on this category
+            setSessionFlash('msg', 'Lĩnh vực đang còn khóa học.');
+            setSessionFlash('msg_type', 'danger');
+            redirect('?module=course_category&action=list');
+        } else {
+            // Delete category
+            $condition = "id=" . $cateID;
+            $deleteStatus = delete('course_category', $condition);
+
+            if ($deleteStatus) {
+                setSessionFlash('msg', 'Xóa thành công.');
+                setSessionFlash('msg_type', 'success');
+                redirect('?module=course_category&action=list');
+            }
+        }
+    } else {
+        setSessionFlash('msg', 'Danh mục không tồn tại.');
+        setSessionFlash('msg_type', 'danger');
+        redirect('?module=course_category&action=list');
+    }
+} else {
+    setSessionFlash('msg', 'Danh mục không tồn tại.');
+    setSessionFlash('msg_type', 'danger');
+}
